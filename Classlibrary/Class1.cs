0decc7cf4feeaabd6dcb24eabd5f24b5ed6f085f
Shivam Kumar <115875580+Badshahshvm@users.noreply.github.com>
2025-10-03 10:01:23 +0530

    Create Class1.cs

---
+++ Classlibrary/Class1.cs
@@ -0,0 +1,11 @@
+namespace ClassLibrary4
+{
+    public class Class1
+    {
+        public static void initCommand()
+        {
+            Console.WriteLine("Init Command is Called");
+        }
+
+    }
+}
